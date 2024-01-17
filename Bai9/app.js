9d9fcdb3eaec133dd411ed9b4ac6105df87b2c64
LAPTOP-G4JJ8UTM\Phan_Hoang_Khang <KhangPhan1121>
2024-01-17 15:48:39 +0700

    Add

--- Bai9/app.js
+++ Bai9/app.js
@@ -1,15 +1,16 @@
+"use strict";
 var search = document.querySelector('.search');
 var city = document.querySelector('.city');
 var country = document.querySelector('.country');
-var value = document.querySelector('.value');
+var value = document.querySelector('.temperature');
 var shortDesc = document.querySelector('.short-desc');
 var visibility = document.querySelector('.visibility span');
 var wind = document.querySelector('.wind span');
 var sun = document.querySelector('.sun span');
-var value = document.querySelector('.value');
 var time = document.querySelector('.time');
 var content = document.querySelector('.content');
 var body = document.querySelector('body');
+
 
 async function changeWeatherUI(capitalSearch) {
     let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${capitalSearch}&appid=0a67d3e3297778cae75529abeb2d294f`
@@ -21,10 +22,12 @@ async function changeWeatherUI(capitalSearch) {
         visibility.innerText = data.visibility + 'm'
         wind.innerText = data.wind.speed + 'm/s'
         sun.innerText = data.main.humidity + '%'
-        let temp = Math.round(data.main.temp - 273.15) + '°C'
-        value.innerText = temp
-        shortDesc.innerText = data.weather[0] ? data.weather[0].main: ''
-        time.innerText = new Date().toLocaleString('vi')
+        let temp = Math.round(data.main.temp - 273.15)
+        value.innerText = temp + '°C'
+        shortDesc.innerText = data.weather[0] ? data.weather[0].main: '';
+        var d = new Date(Date.now() - 7 * 3600000 + data.timezone * 1000) ;
+        time.innerText = d.toLocaleString('vi')
+        
 
         body.setAttribute('class', 'hot')
         if(temp <= 25){
@@ -48,4 +51,3 @@ search.addEventListener('keypress', function(e){
     }
 })
 
-changeWeatherUI()
